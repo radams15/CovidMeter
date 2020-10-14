ebe4fe8dbdf59054fcba9d60a14e32431f334fa6
rhys <email.curly@gmail.com>
2020-10-14 17:40:25 +0100

    Preparing to add wiringPi

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "covid.c"
 
 #define AREA "Dover"
-
 #define DAY (60*60*24)
 
-int* bin(int n){ 
-    // array to store binary number 
-    
-    int* binary = calloc(8, sizeof(int));; 
+int* bin(int n){
+    int* binary = calloc(8, sizeof(int));
     
     if(n > pow(2, 8)-1){
         return binary;
     }
   
-    // counter for binary array 
-    int i = 0; 
+    int i = 0;
     while (n > 0) { 
         binary[7-i] = n % 2; 
         n = n / 2; 
@@ -24,9 +21,13 @@ int* bin(int n){
     }
     
     return binary;
-} 
+}
 
-int main() {
+void set_leds(int* values){
+
+}
+
+int main(){
     time_t secs = time(NULL)-DAY;
 
     struct covid_data data = get_data(AREA, localtime(&secs));
@@ -34,12 +35,8 @@ int main() {
     printf("New Cases: %d\n", data.today_cases);
     
     int* binary = bin(data.today_cases);
-    
-    for(int i=0 ; i<8 ; i++){
-        printf("%d", binary[i]);
-    }
-    free(binary);
-    printf("\n");
+
+    set_leds(binary);
 
     return 0;
 }
